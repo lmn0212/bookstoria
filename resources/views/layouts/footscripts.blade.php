22e1a42cef2dd50a9cb6c90ffc67fb8b4c763b69
lahno <lahno.dima@gmail.com>
2018-06-19 16:38:12 +0300

    fix

--- resources/views/layouts/footscripts.blade.php
+++ resources/views/layouts/footscripts.blade.php
@@ -1,8 +1,4 @@
 <!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-
-<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-<script src="{{asset('slick/slick.js')}}" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript">
     $(document).on('ready', function() {
         $('.regular').slick({
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,19 +8,23 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap-grid.min.css"/>
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap-reboot.min.css"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-grid.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-reboot.min.css')}}"/>
     <!-- Custom style -->
-    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
-    <link rel="stylesheet" type="text/css" href="/css/animate.css"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/animate.css')}}"/>
     <!-- fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Gabriela" rel="stylesheet">
     <!-- карусель -->
-    <link rel="stylesheet" type="text/css" href="/slick/slick.css">
-    <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('slick/slick.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('slick/slick-theme.css')}}">
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <script src="{{asset('slick/slick.js')}}" type="text/javascript" charset="utf-8"></script>
 
 
     <title>Bookstoria.net | @if(isset($title) && !empty($title)) {{$title}} @else Читайте с нами! @endif</title>
