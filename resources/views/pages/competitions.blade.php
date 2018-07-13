dccd6129d6c2c0244b8ca5aa8897e4a2dda74bc2
lahno <lahno.dima@gmail.com>
2018-07-13 14:51:26 +0300

    fix Competitions page

--- resources/views/pages/competitions.blade.php
+++ resources/views/pages/competitions.blade.php
@@ -92,7 +92,7 @@
                             </div>
 
                             <div class="modal-body">
-                                {{$c->rules}}
+                                {!! $c->rules !!}
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Назад</button>
